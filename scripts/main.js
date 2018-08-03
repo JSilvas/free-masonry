20e05bebee48834b96c50a97435157c70843c776
Jay Silvas <jayinlava@gmail.com>
2018-08-03 01:23:13 -0700

    added item size randomization functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function randomNum(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomRatio() { 
+  return { width: randomNum(150, 400), height: randomNum(150, 400) };
+}
+
+function assignItemSize() {
+  
+}
+
+console.log(randomNum(150, 400));
+console.log(randomRatio());
