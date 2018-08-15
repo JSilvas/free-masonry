dcac33e856d54e6f670d51c569c3979a6e52aa6f
Jay Silvas <jayinlava@gmail.com>
2018-08-14 23:30:10 -0700

    initial setup of css based grid and placeholder data

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,38 @@ function randomNum(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function randomRatio() { 
+function randomRatio() {
   return { width: randomNum(150, 400), height: randomNum(150, 400) };
 }
 
 function assignItemSize() {
-  
+
 }
+
+
+const allItems = [];
+
+
+function renderItems() {
+  
+  let galleryEl = document.getElementById('gallery');
+  
+  let itemCount = 0;
+  
+  while (itemCount <= 12) { // for loop to generate items
+    itemCount += 1;
+    
+    let itemEl = document.getElementById('item'); // create new html item
+    // itemEl.textContent('');
+
+    itemEl.setAttribute('height', randomRatio().height);  // assignItemSize
+
+    galleryEl.appendChild(itemEl);  // return new item
+  }
+}
+
 
 console.log(randomNum(150, 400));
 console.log(randomRatio());
+
+renderItems();
